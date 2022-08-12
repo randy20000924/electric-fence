c3b96d42290b6ece5eb171ff16f64f8eebb9216a
Dennis <dennis1989999@gmail.com>
2022-08-12 20:37:02 +0800

    Weng 20220812

---
+++ src/BtnGroup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const btnStyle = {
+    position: 'fixed',
+    top:0,
+    right:0
+
+}
+
+const BtnGroup = () => {
+  return (
+    <button style={btnStyle}>
+
+    </button>
+  )
+}
+
+export default BtnGroup
